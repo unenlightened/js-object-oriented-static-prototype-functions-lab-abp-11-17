0bf23f652007fd64d2972bec6a990efccca6ef08
Chely Ho <adept_inept@yahoo.com>
2017-11-10 02:15:41 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emailList){
+  return emailList.map(function(email){
+    return new IcebreakerResponse(email);
+  })
+}
