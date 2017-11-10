151400990ccf4cf75c07d1a1dae0e4dc6ec556c7
Chely Ho <adept_inept@yahoo.com>
2017-11-10 02:15:18 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,9 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-IcebreakerResponse.BatchCreate = function(emails){
-  return emails.map(function(email){
+IcebreakerResponse.BatchCreate = function(emailList){
+  return emailList.map(function(email){
     return new IcebreakerResponse(email);
   })
 }
-
